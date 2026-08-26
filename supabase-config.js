15d041adaa8b14bd5fec08c2ed06b741e2bc793b
Yusuf Gurcan <yusuf.gurcan.34@hotmail.com>
2026-08-26 15:29:12 +0300

    config: connect supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-window.SUPABASE_URL = 'https://YOUR_PROJECT.supabase.co';
-window.SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+window.SUPABASE_URL = 'https://ihpewhqfkrzmhaivrxzs.supabase.co';
+window.SUPABASE_ANON_KEY = 'sb_publishable_cBxaEJ_wZUGp_9cUNMw1mQ_xL4EyKKK';
