f372b92da25a468245c5f4185985ef615834653f
Yusuf Gurcan <yusuf.gurcan.34@hotmail.com>
2026-08-27 15:31:43 +0300

    fix: add books api end app url

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,4 @@
 window.SUPABASE_URL = 'https://ihpewhqfkrzmhaivrxzs.supabase.co';
 window.SUPABASE_ANON_KEY = 'sb_publishable_cBxaEJ_wZUGp_9cUNMw1mQ_xL4EyKKK';
-window.GOOGLE_BOOKS_API_KEY = '';
+window.GOOGLE_BOOKS_API_KEY = 'AIzaSyC7gtD8-OiYtpbpG8svPJkhjaWM0NxpDRE';
+window.APP_URL = 'https://sitenizin-adresi.com';
